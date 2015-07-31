3548e1685f1ac8354df222c497c01deb18ff589c
Hack Reactor Students <accounts@hackreactor.com>
2015-07-30 17:10:59 -0700

    Enable hashing, comparing password functions; create short links, add visits number

--- app/config.js
+++ app/config.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
 
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
@@ -20,28 +23,34 @@ var linksSchema = mongoose.Schema({
   visits: Number
 });
 
-// userSchema.methods.comparePassword = function(attemptedPassword, callback) {
-//   bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
-//     callback(isMatch);
-// });
-// }
+userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+}
 
-// userSchema.methods.hashPassword = function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.password), null, null).bind(this)
-//       .then(function(hash) {
-//         this.password = hash;
-//       });
-//   }
+userSchema.methods.hashPassword = function(callback){
+  console.log('hashing')
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      console.log('done hashing')
+      this.password = hash;
+      console.log(this);
+      callback();
+    });
+}
+
 exports.User = mongoose.model('User', userSchema);
 
-// linksSchema.methods.shortenLink = function(model, attrs, options){
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(this.url);
-//       this.code = shasum.digest('hex').slice(0, 5));
-// }
+linksSchema.methods.shortenLink =  function(url){
+      var shasum = crypto.createHash('sha1');
+      shasum.update(url);
+      this.code = shasum.digest('hex').slice(0, 5);
+}
 
 exports.Links = mongoose.model('Links', linksSchema);
+
 
 
 
